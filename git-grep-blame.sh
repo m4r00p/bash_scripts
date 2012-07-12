011fa62189a85a2b3946c57759bca61b560145cd
Marek Pawlowski <marekapawlowski@gmail.com>
2012-07-12 22:27:06 +0200

    Fixed one line display executed as bash script

--- git-grep-blame.sh
+++ git-grep-blame.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-if [ ! $# == 1 ]; then
-  echo "U should provide pattern"
-  echo "eg. \n"
-  echo "sh git-grep-blame console.log"
+if [ ! $# == 2 ]; then
+  echo "U should provide pattern and dir. e.g.:"
+  echo "sh git-grep-blame console.log js/"
   exit
 fi
 
-git grep --no-color -n $1 js/ | while IFS=: read i j k; do echo -n "$i:$j"; git blame --color -L $j,+1 $i; done
+git grep --no-color -n $1 $2 | while IFS=: read i j k; do printf "$i:$j\r"; git blame --color -L $j,+1 $i; done
